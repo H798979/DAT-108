84204a47cbdb363e78b18abe469b83e5d4d00134
LPH <LPH@HVL-302023.home>
2025-08-24 16:19:53 +0200

    Løsningsforslag oppgave 456 uke34

---
+++ oppgaver-uke34-456-losning/src/no/hvl/dat108/oppg5filmer/Film.java
@@ -0,0 +1,5 @@
+package no.hvl.dat108.oppg5filmer;
+
+public record Film(String tittel, String regissor, 
+			int utgivelsesaar, double imdbRating) {
+}
---
+++ oppgaver-uke34-456-losning/src/no/hvl/dat108/oppg5filmer/FilmAsClass.java
@@ -0,0 +1,40 @@
+package no.hvl.dat108.oppg5filmer;
+
+public class FilmAsClass {
+	
+	private final String tittel;
+	private final String regissor;
+	private final int utgivelsesaar;
+	private final double imdbRating;
+	
+	public FilmAsClass(String tittel, String regissor, 
+			int utgivelsesaar, double imdbRating) {
+		this.tittel = tittel;
+		this.regissor = regissor;
+		this.utgivelsesaar = utgivelsesaar;
+		this.imdbRating = imdbRating;
+	}
+
+	public String getTittel() {
+		return tittel;
+	}
+
+	public String getRegissor() {
+		return regissor;
+	}
+
+	public int getUtgivelsesaar() {
+		return utgivelsesaar;
+	}
+
+	public double getImdbRating() {
+		return imdbRating;
+	}
+
+	@Override
+	public String toString() {
+		return "Film [tittel=" + tittel + ", regissor=" 
+				+ regissor + ", utgivelsesaar=" + utgivelsesaar
+				+ ", imdbRating=" + imdbRating + "]";
+	}
+}
